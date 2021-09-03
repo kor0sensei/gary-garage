3d1d864d289c6fe09d0aca80d50af01e43f033f2
kor0sensei <averyolmstead@comcast.net>
2021-09-03 13:44:20 -0500

    added all data and cleared all console warnings

---
+++ Zero.cs
@@ -0,0 +1,13 @@
+namespace Garage
+{
+    public class Zero: Vehicle  // Electric motorcycle
+    {
+        public double BatteryKWh { get; set; }
+
+        public void ChargeBattery()
+        {
+            // method definition omitted
+        }
+    }
+}
+
